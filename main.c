9bab3ff08d35789d8ab938395f23d70e4707ce28
LeoCrge <leo.carouge7@gmail.com>
2024-03-29 10:46:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,4 +2,12 @@
 // Created by Carouge on 29/03/2024.
 //
 
+#define REALOC_SIZE 256
 
+
+typedef struct {
+    char *title;
+    int *data;
+    int physical_size;
+    int logical_size;
+} COLUMN;
