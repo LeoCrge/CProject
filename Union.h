ea4eddd7f53a24e290b7c85ba31ceb15d8c32f32
LeoCrge <leo.carouge7@gmail.com>
2024-05-16 18:49:43 +0200

    union part two

---
+++ Union.h
@@ -0,0 +1,37 @@
+//
+// Created by Carouge on 16/05/2024.
+//
+
+#ifndef CPROJECT_UNION_H
+#define CPROJECT_UNION_H
+
+enum enum_type
+{
+    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+};
+typedef enum enum_type ENUM_TYPE;
+
+union column_type{
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
+};
+typedef union column_type COL_TYPE ;
+
+struct column {
+    char *title;
+    unsigned int size; //logical size
+    unsigned int max_size; //physical size
+    ENUM_TYPE column_type;
+    COL_TYPE **data; // array of pointers to stored data
+    unsigned long long int *index; // array of integers
+};
+typedef struct column COLUMN;
+
+
+
+#endif //CPROJECT_UNION_H
