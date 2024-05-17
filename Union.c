7d557c189e40fece1d5bbfafa81f661052065c3a
Silver.btc <silvernight.luther@gmail.com>
2024-05-17 08:28:12 +0200

    add insert_value function

--- Union.c
+++ Union.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define REALLOC_SIZE 256
+
 
 /*
     - Create a new column
@@ -29,6 +31,27 @@ COLUMN *create_column1(ENUM_TYPE type, char *title) {
     return new_column;
 }
 
+/**
+* @brief: Insert a new value into a column
+* @param1: Pointer to the column
+* @param2: Pointer to the value to insert
+* @return: 1 if the value is correctly inserted 0 otherwise
+*/
+int insert_value(COLUMN *col, void *value) {
+    if (!col) return 0;
+    if (col->size <= col->max_size){
+        int* loc = realloc(col->data, (col->size + REALLOC_SIZE) * sizeof(int));
+        if (loc == NULL){
+            printf("Memory allocation failed \n");
+            return 0;
+        }
+        col->data = loc;
+        col->size += REALLOC_SIZE;
+    }
+    col->data[col->max_size++] = value;
+    return 1;
+}
+
 
 int main() {
     COLUMN *mycol = create_column1(CHAR, "caca");
@@ -36,6 +59,11 @@ int main() {
         printf("Column created successfully!\n");
         printf("Title: %s\n", mycol->title);
         printf("Type: %d\n", mycol->column_type);
+
+        char a = 'A', c = 'C';
+        insert_value(mycol, &a);
+        insert_value(mycol, NULL);
+        insert_value(mycol, &c);
 
         free(mycol->title);
         free(mycol);
