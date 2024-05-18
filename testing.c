0917bcaae5760fd5a090276eb8725f0e7bc8767e
Silver.btc <silvernight.luther@gmail.com>
2024-05-18 17:56:54 +0200

    comment testing main

--- testing.c
+++ testing.c
@@ -7,38 +7,38 @@
 #include "Column.h"
 
 
-/*
-int main() {
-    COLUMN *temperature = create_column("Temperature");
 
-    insert_value(temperature, 25);
-    insert_value(temperature, 28);
-    insert_value(temperature, 30);
-    insert_value(temperature, 27);
-    insert_value(temperature, 22);
+// int main() {
+//     COLUMN *temperature = create_column("Temperature");
 
-    printf("Temperature column:\n");
-    print_col(temperature);
+//     insert_value(temperature, 25);
+//     insert_value(temperature, 28);
+//     insert_value(temperature, 30);
+//     insert_value(temperature, 27);
+//     insert_value(temperature, 22);
 
-    printf("\nNumber of occurrences of 28: %d\n", num_occurrences(temperature, 28));
-    printf("Value at position 3: %d\n", value_at_x(temperature, 3));
-    printf("Number of values greater than 25: %d\n", val_greater_x(temperature, 25));
-    printf("Number of values smaller than 25: %d\n", val_smaller_than(temperature, 25));
-    printf("Number of values equal to 25: %d\n", values_equal_to(temperature, 25));
+//     printf("Temperature column:\n");
+//     print_col(temperature);
 
-    CDATAFRAME *dataframe = create_dataframe(1, 5);
+//     printf("\nNumber of occurrences of 28: %d\n", num_occurrences(temperature, 28));
+//     printf("Value at position 3: %d\n", value_at_x(temperature, 3));
+//     printf("Number of values greater than 25: %d\n", val_greater_x(temperature, 25));
+//     printf("Number of values smaller than 25: %d\n", val_smaller_than(temperature, 25));
+//     printf("Number of values equal to 25: %d\n", values_equal_to(temperature, 25));
 
-    dataframe->columns[0] = temperature;
+//     CDATAFRAME *dataframe = create_dataframe(1, 5);
 
-    printf("\nDisplaying the dataframe:\n");
-    display_dataframe(dataframe);
+//     dataframe->columns[0] = temperature;
 
-    int search_result = search_value(dataframe, 30);
-    printf("\nNumber of occurrences of value 30 in the dataframe: %d\n", search_result);
+//     printf("\nDisplaying the dataframe:\n");
+//     display_dataframe(dataframe);
 
-    delete_column(&temperature);
-    free(dataframe->columns);
-    free(dataframe);
+//     int search_result = search_value(dataframe, 30);
+//     printf("\nNumber of occurrences of value 30 in the dataframe: %d\n", search_result);
 
-    return 0;
-}*/
+//     delete_column(&temperature);
+//     free(dataframe->columns);
+//     free(dataframe);
+
+//     return 0;
+// }
